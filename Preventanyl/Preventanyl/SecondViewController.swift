ef3efc11fdc60f67d46a87698c4d85fba7e3db3a
Underrateddev <rajyudhvir29@gmail.com>
2017-09-28 18:02:54 -0700

    Initalised the project, Target iOS 11, Tabbed Layout

---
+++ Preventanyl/Preventanyl/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Preventanyl
+//
+//  Created by Yudhvir Raj on 2017-09-28.
+//  Copyright © 2017 Yudhvir Raj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
